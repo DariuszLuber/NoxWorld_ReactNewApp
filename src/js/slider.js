9ab9d0c90c4dce791752da8dcd1d2571e20bc149
Dariusz Luber <dariusz.luber@gmail.com>
2022-05-31 18:34:20 +0200

    sfeedback

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,37 @@
 import Carousel from 'nuka-carousel';
 import Header from "./header";
 
+
 function Karuzela() {
-    const path1 = require('../assets/slide1.jpg');
-    const path2 = require('../assets/slide2.jpg');
-    const path3 = require('../assets/slide3.jpg');
-    const container = document.getElementById('app');
-    const headerInnerWidth = container.header.innerWidth
-    console.log(headerInnerWidth);
+    const images = [
+        {
+            path: require('../assets/slide1.jpg'),
+            height: '200px'
+        },
+        {
+            path: require('../assets/slide2.jpg'),
+            height: '300px'
+        },
+        {
+            path: require('../assets/slide3.jpg'),
+            height: '400px'
+        },
+    ]
+
+    // you shouldn't use basic JS dom manipulation in React
+    // const container = document.getElementById('app');
+    // const headerInnerWidth = container.header.innerWidth
+    // console.log(headerInnerWidth);
+    
     const styleImg = {
         width: '100%',
-        height: `calc(100vh - ${headerInnerWidth}px)`,
         imageCover: 'cover'
     }
 
+    
     return (
-        <Carousel
-
-        >
-            <img style={styleImg} src={path1} />
-            <img style={styleImg} src={path2}/>
-            <img style={styleImg} src={path3} />
+        <Carousel>
+            {images.map((image, index) => <img key={index} style={{ ...styleImg, height: image.height }} src={path1} /> )}
         </Carousel>
     )
 }
